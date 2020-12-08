bfebdb5d5643b263daf104f80877bf0dd33cf5b5
sstefdev <sstefandev@gmail.com>
2020-12-08 01:31:02 +0100

    Fixed some API problems and added some features

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather("Nis", "Serbia");
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
@@ -10,7 +13,7 @@ function getWeather() {
   weather
     .getWeather()
     .then((results) => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch((err) => console.log(err));
 }
---
+++ ui.js
@@ -0,0 +1,22 @@
+class UI {
+  constructor() {
+    this.location = document.querySelector("#w-location");
+    this.desc = document.querySelector("#w-desc");
+    this.string = document.querySelector("#w-string");
+    this.icon = document.querySelector("#w-icon");
+    this.humidity = document.querySelector("#w-humidity");
+    this.feelsLike = document.querySelector("#w-feelslike");
+    this.wind = document.querySelector("#w-wind");
+  }
+
+  paint(weather) {
+    this.location.textContent = weather.location.name;
+    this.desc.textContent = weather.current.condition.text;
+    this.string.textContent = weather.current.temp_c + "℃";
+    this.icon.setAttribute("src", weather.current.condition.icon);
+    this.humidity.textContent = "Humidity: " + weather.current.humidity;
+    this.feelsLike.textContent =
+      "Feels Like: " + weather.current.feelslike_c + "℃";
+    this.wind.textContent = "Wind: " + weather.current.wind_kph + "/kph";
+  }
+}
